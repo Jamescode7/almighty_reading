4903384b58eaedade6cb79bc80ffe10cafa1cfd9
SeungHwaSong <jamescode7@gmail.com>
2024-01-29 15:28:50 +0900

    url에 week_test 추가

--- manager/urls.py
+++ manager/urls.py
@@ -4,7 +4,7 @@ from appapi.views import flash_version_check
 from manager.views import dashboard, basic_table, profile, interpretation, \
     comprehension, week, week_up, print_page, answer_page, WeekListView, info, agency, \
     downapp, day, plan_info, reportcard, plan_view, reportall, reportSelect, btns, test, test2, reportall_test, \
-    reportcard_test
+    reportcard_test, week_test
 
 app_name = "manager"
 
@@ -25,6 +25,9 @@ urlpatterns = [
 
     path('week_up/', week_up, name='week_up'),
     path('week_up/<int:prev_dt>', week_up, name='week_up'),
+
+    #Week Test
+    path('week_test/', week_test, name='week_test'),
 
     path('day/', day, name='day'),
 
