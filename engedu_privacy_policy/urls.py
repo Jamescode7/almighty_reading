944ef7838d56811fca97f11c216bbd99d9316ae7
SeungHwaSong <jamescode7@gmail.com>
2025-05-19 19:59:44 +0900

    포겟미낫 개인정보취급방침 url 수정 - /forget_me_not 추가

--- engedu_privacy_policy/urls.py
+++ engedu_privacy_policy/urls.py
@@ -4,5 +4,5 @@ from .views import fmn_privacy_policy_view
 app_name = 'engedu_privacy_policy'
 
 urlpatterns = [
-    path('', fmn_privacy_policy_view, name='fmn_privacy_policy_view'),
+    path('forget_me_not', fmn_privacy_policy_view, name='fmn_privacy_policy_view'),
 ]
