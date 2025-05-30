3bc21f5ba738e12d60ddac4af8599df0c9da83ee
SeungHwaSong <jamescode7@gmail.com>
2025-05-30 16:29:14 +0900

    docker 로컬 컨테이너 실행을 위한 처리

--- almighty_reading/settings/deploy.py
+++ almighty_reading/settings/deploy.py
@@ -2,7 +2,7 @@ from .base import *
 
 
 def read_secret(secret_name):
-    file = open('/run/secrets/' + secret_name)
+    file = open("/run/secrets/" + secret_name)
     secret = file.read()
     secret = secret.rstrip().lstrip()
     file.close()
@@ -15,35 +15,34 @@ env = environ.Env(
 )
 
 # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settingspp - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+SECRET_KEY = read_secret("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://www.readoly.co.kr']
+CSRF_TRUSTED_ORIGINS = ["https://www.readoly.co.kr"]
 
 # HTTPS 사용 시 Django가 'X-Forwarded-Proto' 헤더를 신뢰할 수 있도록 설정
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': read_secret('MYSQL_PASSWORD'),
-        'HOST': 'mariadb',
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django",
+        "USER": "django",
+        "PASSWORD": read_secret("MYSQL_PASSWORD"),
+        "HOST": "mariadb",
+        "PORT": "3306",
     }
 }
-
--- almighty_reading/settings/local.py
+++ almighty_reading/settings/local.py
@@ -1,32 +1,30 @@
 from .base import *
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# env = environ.Env(
+# set casting, default value
+#  DEBUG=(bool, False)
+# )
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, ".env.local"))
 
 # Quick-start development settingspp - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ["*"]
 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
