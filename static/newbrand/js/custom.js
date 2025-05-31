416607362791345cd4aa115cb18570faf7203dc1
SeungHwaSong <jamescode7@gmail.com>
2025-05-31 16:41:39 +0900

    newbrand-product 개선

--- static/newbrand/js/custom.js
+++ static/newbrand/js/custom.js
@@ -1,12 +1,15 @@
-// Smooth scroll for internal links (Navbar → Anchor)
 $(function () {
-  $('a[href^="#"]').on('click', function (e) {
+  // 스크롤 시 .js-scroll-trigger 클래스가 붙은 링크에 대해 부드러운 스크롤 적용
+  $('a.js-scroll-trigger[href^="#"]').on("click", function (e) {
     var target = $(this.hash);
     if (target.length) {
       e.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top - 70
-      }, 600);
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top - 70, // 네비게이션 바 높이 만큼 보정
+        },
+        600
+      );
     }
   });
 });
